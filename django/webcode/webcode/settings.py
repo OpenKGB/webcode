43b200639c0f6e5aa52dacffebb2c7d5313e2e77
Boris A. Burkov <vasjaforutube@gmail.com>
2017-08-12 18:53:42 +0100

    Added static configuration to django.

--- django/webcode/webcode/settings.py
+++ django/webcode/webcode/settings.py
@@ -115,5 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "webcode", "frontend", "dist"),
+)
 from .local_settings import *
