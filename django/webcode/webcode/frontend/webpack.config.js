adc0e019a0f84e9d9e01e6d2f5ae0abaa667a42a
Boris A. Burkov <vasjaforutube@gmail.com>
2017-08-31 14:03:44 +0100

    Edited webpack output path to /static/.

--- django/webcode/webcode/frontend/webpack.config.js
+++ django/webcode/webcode/frontend/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'app.jsx'),
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/static/',
     filename: 'app.[hash:7].js'
   },
   resolve: {
